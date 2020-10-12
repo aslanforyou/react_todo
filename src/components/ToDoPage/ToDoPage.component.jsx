882bcc4f2c76fe516e62afab80d092cc1d7530cb
aslan <aslanforyou@mail.ru>
2020-10-12 12:53:42 +0300

    simple input

--- src/components/ToDoPage/ToDoPage.component.jsx
+++ src/components/ToDoPage/ToDoPage.component.jsx
@@ -1,16 +1,18 @@
 import React, {Component} from "react";
 import Header from "./components/Header/Header.Component";
 import Footer from "./components/Footer/Footer.Component";
+import './ToDoPage.css';
+import Input from "./components/Input/Input.Component";
 
 class ToDoPage extends Component {
   render() {
     return (
       <div>
-        <Header title='My Todos app'/>
-        <div>
-
+        <Header title='My todo list'/>
+        <div className="container">
+          <Input/>
+          <Footer/>
         </div>
-        <Footer/>
       </div>
     )
   }
